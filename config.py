d9c571202be0f438baa3e76ded111ecfb9ec823d
Speedwolf02 <158140629+Speedwolf02@users.noreply.github.com>
2025-11-23 18:45:30 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,12 +1,12 @@
-api_id = 10811400
+api_id =  24435985
 
-api_hash = "191bf5ae7a6c39771e7b13cf4ffd1279"
+api_hash = "0fec896446625478537e43906a4829f8"
 
-bot_token = "7412278588:AAHtejVjcX0W7dvtHCDs0Py6CmRuvGDYLCw"
+bot_token = "7758738938:AAGwhb8vXtHw9INX8SzCr82PKYtjQJHE-3c"
 
 GROUP_TAG = "SUNRISES2RIPS"
 
-sudo_users = [6469754522]
+sudo_users = [6299192020]
 
 DL_DONE_MSG = """
 ✅ <b> {} Completed In </b> <code>{}</code>
